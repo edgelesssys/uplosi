c98f3f924e70a484dd03d77b4f743aafc78f4d45
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-03 11:26:56 +0100

    flake: refactor
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,47 +11,41 @@
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          version = "0.1.2";
-          uplosi = pkgs.buildGo121Module
-            {
-              pname = "uplosi";
-              version = version;
-              src = ./.;
-              # this needs to be updated together with go.mod / go.sum
-              vendorHash = "sha256-RsjUPLe8omoN+XGyNhHDxzNfZR7VVTkh/f/On1oCRqM=";
+          uplosi = pkgs.buildGoModule {
+            pname = "uplosi";
+            version = "0.1.2";
+            src = ./.;
+            # this needs to be updated together with go.mod / go.sum
+            vendorHash = "sha256-RsjUPLe8omoN+XGyNhHDxzNfZR7VVTkh/f/On1oCRqM=";
 
-              preBuild = ''
-                export CGO_ENABLED=0
-              '';
+            CGO_ENABLED = 0;
 
-              ldflags = [ "-s" "-w" "-buildid=" ];
-              flags = [ "-trimpath" ];
+            ldflags = [ "-s" "-w" "-buildid=" ];
 
-              nativeBuildInputs = [ pkgs.installShellFiles ];
+            nativeBuildInputs = [ pkgs.installShellFiles ];
 
-              postInstall = ''
-                installShellCompletion --cmd uplosi \
-                  --bash <($out/bin/uplosi completion bash) \
-                  --fish <($out/bin/uplosi completion fish) \
-                  --zsh <($out/bin/uplosi completion zsh)
-              '';
+            postInstall = ''
+              installShellCompletion --cmd uplosi \
+                --bash <($out/bin/uplosi completion bash) \
+                --fish <($out/bin/uplosi completion fish) \
+                --zsh <($out/bin/uplosi completion zsh)
+            '';
 
-              meta = with pkgs.lib; {
-                description = "Upload OS images to cloud provider";
-                homepage = "https://github.com/edgelesssys/uplosi";
-                maintainers = with maintainers; [
-                  katexochen
-                  malt3
-                ];
-                license = licenses.asl20;
-              };
+            meta = with pkgs.lib; {
+              description = "Upload OS images to cloud provider";
+              homepage = "https://github.com/edgelesssys/uplosi";
+              maintainers = with maintainers; [ katexochen malt3 ];
+              license = licenses.asl20;
             };
+          };
         in
         {
           devShells.default = import ./shell.nix { inherit pkgs; };
 
-          packages.uplosi = uplosi;
-          defaultPackage = uplosi;
+          packages = {
+            default = uplosi;
+            uplosi = uplosi;
+          };
 
           formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
         }
