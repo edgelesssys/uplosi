73b6208ac21603bb69e8079fa8be821e590de286
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-05-17 12:03:17 +0200

    go.mod: Update Go dependencies

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
             version = "0.2.0";
             src = ./.;
             # this needs to be updated together with go.mod / go.sum
-            vendorHash = "sha256-f8Yz99qlN0S0Ybewifc0VQanYXKinb1togBkUwDPSvw=";
+            vendorHash = "sha256-1o30yZCx43FsFn+b6OQgRluJhBD9cSPXpXQv++IvxoI=";
 
             CGO_ENABLED = 0;
 
