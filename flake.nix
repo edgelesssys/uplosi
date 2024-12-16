8449ce2ae2f76b5ebc4020731c242b7a06c0ed3d
Paul Meyer <katexochen0@gmail.com>
2024-12-16 12:12:54 +0100

    treewide: refomat
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- flake.nix
+++ flake.nix
@@ -14,58 +14,68 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          uplosi = pkgs.buildGoModule {
-            pname = "uplosi";
-            version = "devel";
-            src = ./.;
-            # this needs to be updated together with go.mod / go.sum
-            vendorHash = "sha256-2lJmPNLpI1ksFb0EtcjPjyTy7eX1DKeX0F80k9FtGno=";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-            CGO_ENABLED = 0;
+        uplosi = pkgs.buildGoModule {
+          pname = "uplosi";
+          version = "devel";
+          src = ./.;
+          # this needs to be updated together with go.mod / go.sum
+          vendorHash = "sha256-2lJmPNLpI1ksFb0EtcjPjyTy7eX1DKeX0F80k9FtGno=";
 
-            ldflags = [ "-s" ];
+          CGO_ENABLED = 0;
 
-            nativeBuildInputs = [ pkgs.installShellFiles ];
+          ldflags = [ "-s" ];
 
-            postInstall = ''
-              installShellCompletion --cmd uplosi \
-                --bash <($out/bin/uplosi completion bash) \
-                --fish <($out/bin/uplosi completion fish) \
-                --zsh <($out/bin/uplosi completion zsh)
-            '';
+          nativeBuildInputs = [ pkgs.installShellFiles ];
 
-            meta = with pkgs.lib; {
-              description = "Upload OS images to cloud provider";
-              homepage = "https://github.com/edgelesssys/uplosi";
-              maintainers = with maintainers; [ katexochen malt3 ];
-              license = licenses.asl20;
-            };
+          postInstall = ''
+            installShellCompletion --cmd uplosi \
+              --bash <($out/bin/uplosi completion bash) \
+              --fish <($out/bin/uplosi completion fish) \
+              --zsh <($out/bin/uplosi completion zsh)
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Upload OS images to cloud provider";
+            homepage = "https://github.com/edgelesssys/uplosi";
+            maintainers = with maintainers; [
+              katexochen
+              malt3
+            ];
+            license = licenses.asl20;
           };
+        };
 
-          treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-        in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        devShells.default = import ./shell.nix { inherit pkgs; };
 
-          packages = {
-            default = uplosi;
-            uplosi = uplosi;
-          };
+        packages = {
+          default = uplosi;
+          inherit uplosi;
+        };
 
-          legacyPackages = {
-            nixpkgs = nixpkgs.legacyPackages.${system};
-          };
+        legacyPackages = {
+          nixpkgs = nixpkgs.legacyPackages.${system};
+        };
 
-          formatter = treefmtEval.config.build.wrapper;
+        formatter = treefmtEval.config.build.wrapper;
 
-          checks = {
-            formatting = treefmtEval.config.build.check self;
-          };
-        }
-      );
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
+      }
+    );
 }
